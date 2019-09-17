0b52c46a06ae61aa8bbeeb2f1538c3073555ded3
hoanglong <hoangdinhlong2997@gmail.com>
2019-09-17 19:26:08 +0700

    bbbb Signed-off-by: hoanglong <hoangdinhlong2997@gmail.com>

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '115.79.55.95'),
+            'host' => env('DB_HOST', 'remotemysql.com'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'mysql'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'anphat@@123'),
+            'username' => env('DB_USERNAME', '4m38xRVag3'),
+            'password' => env('DB_PASSWORD', 'AyEyFDrYyG'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
