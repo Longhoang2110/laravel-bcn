5ca224f7c618ee98037ff3c2d043a19e9501046d
Hoangngoctrong1997 <trong02071997>
2019-09-24 20:34:13 +0700

    zalo Signed-off-by: Hoangngoctrong1997 <trong02071997>

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -532,23 +532,9 @@
 						</div>
 					</div>
 					<!-- Load Facebook SDK for JavaScript -->
-					<div id="fb-root"></div>
-					<script>
-					  window.fbAsyncInit = function() {
-						FB.init({
-						  appId            : '1386617178176018',
-						  xfbml            : true,
-						  version          : 'v4.0'
-						});
-					  };
-			  
-					  (function(d, s, id) {
-					  var js, fjs = d.getElementsByTagName(s)[0];
-					  if (d.getElementById(id)) return;
-					  js = d.createElement(s); js.id = id;
-					  js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
-					  fjs.parentNode.insertBefore(js, fjs);
-					}(document, 'script', 'facebook-jssdk'));</script>
+					<div class="zalo-chat-widget" data-oaid="579745863508352884" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="350" data-height="420"></div>
+
+					<script src="https://sp.zalo.me/plugins/sdk.js"></script>
 			  
 					<!-- Your customer chat code -->
 					<div class="fb-customerchat"
