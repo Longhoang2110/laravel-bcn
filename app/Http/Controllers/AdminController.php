5f946f90933734532645ff0456ab1e9be08d0d37
Hoangngoctrong1997 <trong02071997>
2019-10-25 15:50:36 +0700

    âs Signed-off-by: Hoangngoctrong1997 <trong02071997>

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,7 +11,7 @@ class AdminController extends Controller
 {
     //
     public function getlogin(){
-         return view('admin.login',compact('buyer'));
+         return view('admin.login');
 
         // $buyer = buyer::all();
         // $buyer = DB::table('buyer')
